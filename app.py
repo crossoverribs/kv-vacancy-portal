3e2753ac547ef2a2c0b3392d78ef6efae6c82d8c
Rahul T H <rahulhari98@gmail.com>
2025-05-30 20:51:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -42,3 +42,14 @@ if __name__ == '__main__':
     init_db()
     port = int(os.environ.get("PORT", 10000))  # Render provides PORT
     app.run(host='0.0.0.0', port=port)
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
